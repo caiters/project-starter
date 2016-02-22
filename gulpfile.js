752f64c63bbeb0f35e655ff9cc412d6bb685aca2
Caitlin <ayashi@valefor.com>
2016-02-21 16:18:16 -0800

    removing unused files and fixing gulpfile (gulp connect has live reload functionality...)

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,6 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var cssnano = require('gulp-cssnano');
 var sourcemaps = require('gulp-sourcemaps');
-var livereload = require('gulp-livereload');
-//var watch = require('gulp-watch');
 var connect = require('gulp-connect');
 var plumber = require('gulp-plumber');
 
@@ -18,7 +16,7 @@ gulp.task('css', function(){
     .pipe(sourcemaps.write('maps'))
     .pipe(plumber.stop())
     .pipe(gulp.dest(cssDest))
-    .pipe(livereload());
+    .pipe(connect.reload());
 });
 
 gulp.task('connect', function() {
@@ -30,7 +28,7 @@ gulp.task('connect', function() {
 
 gulp.task('html', function(){
   gulp.src('./*.html')
-    .pipe(livereload());
+    .pipe(connect.reload());
 });
 
 gulp.task('watch:html', function(){
@@ -38,7 +36,6 @@ gulp.task('watch:html', function(){
 });
 
 gulp.task('watch:css', ['css'], function(){
-  livereload.listen();
   gulp.watch('css/**/*.less', ['css']);
 });
 
